8da36b16adde7981dd0a9a519637ca7879b56448
Cristina Estupiñán <ciestupinan@gmail.com>
2025-08-25 09:41:30 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar'
 
 function App() {
   return (
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,6 +1,14 @@
-const Navbar = () => {
+import { Button } from "../@/components/ui/button";
 
-    return <div></div>
-}
+const Navbar = () => {
+  return (
+    <div className="bg-[var(--color-neutral-100)] w-full pl-[60px] pr-[60px] pt-[20px] pb-[20px]">
+      <div className="flex justify-center gap-[40px]">
+      <div>Home</div>
+      <div>About</div>
+      <div>Recipes</div></div>
+    </div>
+  );
+};
 
 export default Navbar;
