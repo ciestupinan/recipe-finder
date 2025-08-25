5bd62d90761669ebd079d5e2f2671ed453ca474b
Cristina Estupiñán <ciestupinan@gmail.com>
2025-08-25 09:56:15 -0400

    basic navbar, primary button

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,11 +2,16 @@ import { Button } from "../@/components/ui/button";
 
 const Navbar = () => {
   return (
-    <div className="bg-[var(--color-neutral-100)] w-full pl-[60px] pr-[60px] pt-[20px] pb-[20px]">
-      <div className="flex justify-center gap-[40px]">
-      <div>Home</div>
-      <div>About</div>
-      <div>Recipes</div></div>
+    <div className="bg-[var(--color-neutral-100)] w-full flex justify-between pl-[60px] pr-[60px] pt-[20px] pb-[20px]">
+      <div className="flex items-center">Healthy Recipe Finder</div>
+      <div className="flex justify-center items-center gap-[40px]">
+        <div>Home</div>
+        <div>About</div>
+        <div>Recipes</div>
+      </div>
+      <div className="flex items-center">
+        <Button>Browse recipes</Button>
+      </div>
     </div>
   );
 };
