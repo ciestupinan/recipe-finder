6d8c3eb51a5844463f946e2d4aabe8913b0f9743
Cristina Estupiñán <ciestupinan@gmail.com>
2025-08-25 10:00:48 -0400

    logo

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Button } from "../@/components/ui/button";
-
+import Logo from "../src/assets/images/logo.svg";
 const Navbar = () => {
   return (
     <div className="bg-[var(--color-neutral-100)] w-full flex justify-between pl-[60px] pr-[60px] pt-[20px] pb-[20px]">
-      <div className="flex items-center">Healthy Recipe Finder</div>
+      <div className="flex items-center">
+        <img src={Logo} alt="logo" />
+      </div>
       <div className="flex justify-center items-center gap-[40px]">
         <div>Home</div>
         <div>About</div>
