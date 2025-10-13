8fe6874da256d384cd49426b510e9ee9399fa927
Cristina Estupiñán <ciestupinan@gmail.com>
2025-10-13 16:08:55 -0400

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './Navbar'
+import Homepage from './Homepage'
 
 function App() {
   return (
     <>
        <Navbar />
+       <Homepage />
     </>
   )
 }
---
+++ src/Homepage.tsx
@@ -0,0 +1,13 @@
+import { Button } from "../@/components/ui/button";
+
+const Homepage = () => {
+
+    return <div>
+
+        <h1 className='font-nunito'>Start exploring</h1>
+        <p className='font-nunito-sans'>Discover eight quick, whole-food recipes that you can cook tonight—no processed junk, no guesswork.</p>
+        <Button>Start exploring</Button>
+    </div>
+}
+
+export default Homepage;
