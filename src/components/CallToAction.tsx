353a1089e2d581f72bc25f29bdff386f7dbe78af
Cristina Estupiñán <ciestupinan@gmail.com>
2025-10-14 17:44:28 -0400

    menu, install radix

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -4,14 +4,14 @@ const CallToAction = () => {
   return (
     <div className="bg-[var(--color-neutral-100)] w-full flex flex-col justify-center text-center">
       <div className="flex justify-center">
-        <div className="flex bg-[var(--color-neutral-200)] w-[1192px] pt-[var(--spacing-1200)] pb-[var(--spacing-1200)] rounded-xl relative justify-center overflow-hidden z-12">
+        <div className="flex bg-[var(--color-neutral-200)] w-[1192px] pt-[var(--spacing-1200)] pb-[var(--spacing-1200)] rounded-xl relative justify-center overflow-hidden z-8">
           <img
-            className="absolute -bottom-14 -left-14 z-10"
+            className="absolute -bottom-14 -left-14 z-5"
             src="src/assets/images/pattern-fork.svg"
             alt="fork"
           />
           <img
-            className="absolute top-5 -right-20 z-10"
+            className="absolute top-5 -right-20 z-5"
             src="src/assets/images/pattern-knife.svg"
             alt="knife"
           />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Popover } from "radix-ui";
 import { Button } from "../../@/components/ui/button";
 import Logo from "../assets/images/logo.svg";
 
@@ -9,21 +10,55 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="bg-[var(--color-neutral-100)] border-b-1 border-[var(--color-neutral-300)] w-full flex justify-between pl-[60px] pr-[60px] pt-[20px] pb-[20px]">
-      <div className="flex items-center">
-        <img src={Logo} alt="logo" />
+    <>
+      <div className="bg-[var(--color-neutral-100)] border-b-1 border-[var(--color-neutral-300)] w-full flex justify-between lg:px-(--spacing-800) lg:py-(--spacing-250) p-(--spacing-400)">
+        <div className="flex items-center">
+          <img src={Logo} alt="logo" />
+        </div>
+        <ul className="flex justify-center items-center gap-[40px] hidden lg:flex">
+          {menuItems.map((item) => (
+            <li>
+              <h6>{item.title}</h6>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden lg:flex">
+          <div className="flex items-center">
+            <Button>Browse recipes</Button>
+          </div>
+        </div>
+        <div className="lg:hidden sm:flex">
+          <Popover.Root>
+            <Popover.Trigger className="PopoverTrigger">
+              <div className="flex bg-[var(--color-neutral-200)] rounded-md w-[40px] h-[40px] justify-center items-center">
+                <img
+                  className="flex w-[18px]"
+                  src="src/assets/images/icon-hamburger-menu.svg"
+                  alt="menu"
+                />
+              </div>
+            </Popover.Trigger>
+            <Popover.Portal>
+              <Popover.Content className="PopoverContent" sideOffset={5} align="end">
+                <div className="flex flex-col bg-[var(--color-neutral-100)] rounded-md w-[325px] p-[var(--spacing-100)] border-1 border-[var(--color-neutral-300)] shadow-2xl">
+                  <ul className="flex flex-col gap-[var(--spacing-150)]">
+                    {menuItems.map((item) => (
+                      <li>
+                        <h6>{item.title}</h6>
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="flex items-center mt-[var(--spacing-150)]">
+                    <Button>Browse recipes</Button>
+                  </div>
+                </div>
+                <Popover.Arrow className="PopoverArrow" />
+              </Popover.Content>
+            </Popover.Portal>
+          </Popover.Root>
+        </div>
       </div>
-      <ul className="flex justify-center items-center gap-[40px]">
-        {menuItems.map((item) => (
-          <li>
-            <h6>{item.title}</h6>
-          </li>
-        ))}
-      </ul>
-      <div className="flex items-center">
-        <Button>Browse recipes</Button>
-      </div>
-    </div>
+    </>
   );
 };
 
