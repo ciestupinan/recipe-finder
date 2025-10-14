dac58e2aaa6c2281ccba0bff8b3a5340b97804f0
Cristina Estupiñán <ciestupinan@gmail.com>
2025-10-14 14:56:40 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,20 @@
 const Footer = () => {
+  const socials = [
+    { name: "Instagram", img: "src/assets/images/icon-instagram.svg" },
+    { name: "TikTok", img: "src/assets/images/icon-tiktok.svg" },
+  ];
 
-    return <div></div>
-}
+  return (
+    <div className="bg-[var(--color-neutral-100)] w-full flex justify-center">
+      <div className="flex pt-[var(--spacing-500)] pb-[var(--spacing-500)] w-[1192px] justify-end">
+        <div className="flex gap-[var(--spacing-300)]">
+        {socials.map((social) => (
+          <img key={social.name} src={social.img} alt={social.name}/>
+        ))}
+      </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
