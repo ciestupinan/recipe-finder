9bbbe60934fe276cb02660173adcf87b29c58dab
Cristina Estupiñán <ciestupinan@gmail.com>
2025-10-14 15:01:07 -0400

    pull out callout items

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,6 +1,26 @@
 import { Button } from "../../@/components/ui/button";
 
 const Homepage = () => {
+  const calloutItems = [
+    {
+      title: "Whole-food recipes",
+      subtitle: "Each dish uses everyday, unprocessed ingredients.",
+      img: "src/assets/images/icon-whole-food-recipes.svg",
+    },
+    {
+      title: "Minimum fuss",
+      subtitle:
+        "All recipes are designed to make eating healthy quick and easy.",
+      img: "src/assets/images/icon-minimum-fuss.svg",
+    },
+    {
+      title: "Search in seconds",
+      subtitle:
+        "Filter by name or ingredient and jump straight to the recipe you need.",
+      img: "src/assets/images/icon-search-in-seconds.svg",
+    },
+  ];
+
   return (
     <div className="bg-[var(--color-neutral-100)] w-full flex flex-col justify-center text-center">
       <div className="mb-[var(--spacing-150)] mt-[var(--spacing-1000)]">
@@ -29,32 +49,15 @@ const Homepage = () => {
         <h2>What you'll get</h2>
       </div>
       <div className="flex justify-center gap-[var(--spacing-400)] text-left mb-[var(--spacing-1200)]">
-        <div className="flex flex-col w-[376px]">
-          <div className="flex bg-[var(--color-neutral-0)] w-[60px] h-[60px] rounded-xl justify-center items-center mb-[var(--spacing-300)]">
-            <img src="src/assets/images/icon-whole-food-recipes.svg" />
+        {calloutItems.map((item) => (
+          <div className="flex flex-col w-[376px]">
+            <div className="flex bg-[var(--color-neutral-0)] w-[60px] h-[60px] rounded-xl justify-center items-center mb-[var(--spacing-300)]">
+              <img src={item.img} />
+            </div>
+            <h3 className="mb-[var(--spacing-150)]">{item.title}</h3>
+            <h5>{item.subtitle}</h5>
           </div>
-          <h3 className="mb-[var(--spacing-150)]">Whole-food recipes</h3>
-          <h5>Each dish uses everyday, unprocessed ingredients.</h5>
-        </div>
-        <div className="flex flex-col w-[376px]">
-          <div className="flex bg-[var(--color-neutral-0)] w-[60px] h-[60px] rounded-xl justify-center items-center mb-[var(--spacing-300)]">
-            <img src="src/assets/images/icon-minimum-fuss.svg" />
-          </div>
-          <h3 className="mb-[var(--spacing-150)]">Minimum fuss</h3>
-          <h5>
-            All recipes are designed to make eating healthy quick and easy.
-          </h5>
-        </div>
-        <div className="flex flex-col w-[376px]">
-          <div className="flex bg-[var(--color-neutral-0)] w-[60px] h-[60px] rounded-xl justify-center items-center mb-[var(--spacing-300)]">
-            <img src="src/assets/images/icon-search-in-seconds.svg" />
-          </div>
-          <h3 className="mb-[var(--spacing-150)]">Search in seconds</h3>
-          <h5>
-            Filter by name or ingredient and jump straight to the recipe you
-            need.
-          </h5>
-        </div>
+        ))}
       </div>
       <div className="flex justify-center mt-[var(--spacing-1200)] mb-[var(--spacing-1200)] text-left">
         <div className="flex w-[1192px] gap-[var(--spacing-600)]">
