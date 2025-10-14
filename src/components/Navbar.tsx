6a823e1fc54bb50f3730b557b9520c578ed74aca
Cristina Estupiñán <ciestupinan@gmail.com>
2025-10-14 15:17:09 -0400

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,25 @@
 import { Button } from "../../@/components/ui/button";
 import Logo from "../assets/images/logo.svg";
+
 const Navbar = () => {
+  const menuItems = [
+    { title: "Home" },
+    { title: "About" },
+    { title: "Recipes" },
+  ];
+
   return (
-    <div className="bg-[var(--color-neutral-100)] w-full flex justify-between pl-[60px] pr-[60px] pt-[20px] pb-[20px]">
+    <div className="bg-[var(--color-neutral-100)] border-b-1 border-[var(--color-neutral-300)] w-full flex justify-between pl-[60px] pr-[60px] pt-[20px] pb-[20px]">
       <div className="flex items-center">
         <img src={Logo} alt="logo" />
       </div>
-      <div className="flex justify-center items-center gap-[40px]">
-        <div>Home</div>
-        <div>About</div>
-        <div>Recipes</div>
-      </div>
+      <ul className="flex justify-center items-center gap-[40px]">
+        {menuItems.map((item) => (
+          <li>
+            <h6>{item.title}</h6>
+          </li>
+        ))}
+      </ul>
       <div className="flex items-center">
         <Button>Browse recipes</Button>
       </div>
