d39db9b94665bf47efa6c8bc15466ee34d4cc9de
Cristina Estupiñán <ciestupinan@gmail.com>
2025-12-18 15:35:34 -0500

    add heading

--- src/components/RecipeListPage/RecipeListPage.tsx
+++ src/components/RecipeListPage/RecipeListPage.tsx
@@ -4,14 +4,28 @@ import Navbar from "../shared/Navbar";
 import RecipeCard from "./RecipeCard";
 
 const RecipeListPage = () => {
-
   return (
     <>
       <Navbar />
-      <div className="flex w-full flex-wrap px-[var(--spacing-1600)] gap-[var(--spacing-400)]">
-      {recipes?.map(r => (
-        <RecipeCard recipe={r} key={r.id}/>
-      ))}
+      <div className="py-[var(--spacing-1000)] flex justify-center gap-[var(--spacing-800)] text-center">
+        <div className="flex flex-col w-[824px] justify-center items-center mb-[var(--spacing-800)]">
+          <h2 className="mb-[var(--spacing-150)]">
+            Explore our simple, healthy recipes
+          </h2>
+          <h5>
+            Discover eight quick, whole-food dishes that fit real-life schedules
+            and taste amazing. Use the search bar to find a recipe by name or
+            ingredient, or simply scroll the list and let something delicious
+            catch your eye.
+          </h5>
+        </div>
+      </div>
+      <div className="flex w-full px-[var(--spacing-1600)]">
+        <div className="flex w-full flex-wrap gap-[var(--spacing-400)]">
+          {recipes?.map((r) => (
+            <RecipeCard recipe={r} key={r.id} />
+          ))}
+        </div>
       </div>
       <Footer />
     </>
